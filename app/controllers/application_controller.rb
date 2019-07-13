128af99ce9c00fdf25e6cf15f9281690956a8ac1
Samuel Cunningham <samuel.p.cunningham@gmail.com>
2019-07-13 16:52:11 +0000

    allowed course creation by creating migration that gave db fields. ran migration and then added associations to class models. generated instructor/courses controller using namespace controller, giving new create show resourses. required user authentication in  courses controller and defined new create and show. added simple form for create course and basic view for show.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+before_action :authenticate_user!
+# this code added made the index page require a sign in to view
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = current_user.courses.create(course_params)
+    redirect_to instructor_course_path(@course)
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
 end
