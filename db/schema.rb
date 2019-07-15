6ef4ac4dc7bbb788509312430e3ecb1ef14b7d93
Samuel Cunningham <samuel.p.cunningham@gmail.com>
2019-07-15 23:19:56 +0000

    issue was because columns not listed in db but because migration was run before I made changes (i think it was originally spelling error) I had to run new migration that added columns to sections field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_191632) do
+ActiveRecord::Schema.define(version: 2019_07_15_231325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_07_13_191632) do
   create_table "sections", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.integer "course_id"
   end
 
   create_table "users", force: :cascade do |t|
