46f37bfd53ebe7a50dd52b627255fc4e257ac788
Samuel Cunningham <samuel.p.cunningham@gmail.com>
2019-07-28 13:57:37 +0000

    added credit card functionality through stripe, set up through lessons controller and set up procedures for premium and free content

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_course, only: [:show]
+
   def show
   end
 
@@ -8,4 +11,13 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def require_enrolled_in_current_course
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to current_lesson.section.course, alert: 'Not enrolled in course, enroll to view content'
+    end
+
+  end
+
+
 end
