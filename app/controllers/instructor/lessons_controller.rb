c5450af3539a4b743add026b6fc077e048d1b4c3
Samuel Cunningham <samuel.p.cunningham@gmail.com>
2019-07-28 23:11:46 +0000

    added drag and drop functionality to both sections and lessons for instructors

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,6 +1,10 @@
 class Instructor::LessonsController < ApplicationController
 before_action :authenticate_user!
-before_action :require_authorized_for_current_section
+before_action :require_authorized_for_current_section, only: [:new, :create]
+before_action :require_authorized_for_current_lesson, only: [:update]
+skip_before_action :verify_authenticity_token
+  #This was the only suggested fix I could find that would fix error received when trying to reorder list items. I got this line from the following stackoverflow post: https://stackoverflow.com/questions/5669322/turn-off-csrf-token-in-rails-3
+  
 
   def new
     @lesson = Lesson.new
@@ -11,7 +15,22 @@ before_action :require_authorized_for_current_section
     redirect_to instructor_course_path(current_section.course)
   end
 
+  def update
+    current_lesson.update_attributes(lesson_params)
+    render plain: 'updated!'
+  end
+
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
@@ -25,6 +44,6 @@ before_action :require_authorized_for_current_section
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video)
+    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end
 end
